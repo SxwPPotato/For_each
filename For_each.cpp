2fae9bc19c76bdab82c2867edef70607c9cf816e
Vitalya <vitalyabes.vb@gmail.com>
2024-04-17 11:29:36 +0300

    Second commit

--- For_each.cpp
+++ For_each.cpp
@@ -1,33 +1,40 @@
 ﻿#include <iostream>
 #include <vector>
 #include <thread>
+#include <algorithm>
+#include <future>
+#include <numeric>
 
 template<typename Func>
 
-Func for_each(std::vector<int>::iterator begin, std::vector<int>::iterator end, Func&& function) {
-
-    while (begin < end) {
-        function(begin);     
-        begin = begin + 1;
-        return for_each(begin, end, function);
-
+Func par_for_each(std::vector<int>::iterator begin, std::vector<int>::iterator end, Func function) {
+   
+    const auto diff = std::distance(begin, end);
+    std::cout << diff << std::endl;
+    if (diff < 11) {
+      return std::for_each(begin, end, function);
+       
     }
+    
+    auto middle = begin;
+    std::advance(middle, diff / 2);
 
+    auto fut_res = std::async(par_for_each<Func>, begin, middle, function);
+
+    auto lf_res = par_for_each(middle, end, function);
+    return fut_res.get();
+    
 }
 
 int main()
 {
     setlocale(LC_ALL, "russian");
 
-    std::vector<int> vec{ 3,7,4,9,1,3,5,6,2 };
+    std::vector<int> vec{ 3,7,4,9,1,3,5,6,2,9,5,8,1 };
 
 
-    for_each(vec.begin(), vec.end(), [](std::vector<int>::iterator begin) {
-        std::cout << *begin << " ";
+    par_for_each(vec.begin(), vec.end(), [](int begin) {
+        std::cout << begin << " ";
        });
-
-    
-
-
     return 0;
 }
